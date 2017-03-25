f1a6f6da0e9f09f39a15a1ba50571c3fa76ca5a1
Lavee Singh <laveechaudharytheone@gmail.com>
2017-03-25 20:52:43 +0530

    Store details saved as lists

--- util/store.py
+++ util/store.py
@@ -19,17 +19,14 @@ def decode(encoded):
     decoded = base64.b64decode(encoded)
     return zlib.decompress(decoded)
 
-HEADER = "eNotjlsOwzAIBC+0n1XuQxziUGETAenj9o2t/uyMxJMi2BPF2qn8QcnvyWD3b\
-                ti5v7CrUUJ6jkJAwpbHApUmGVArpIxGeSA4n+1ESO2kiNxIb53idSKtSRm2ms\
-                2OjfeBe/mEjVRZB7o55+Vj3qVXZJ1H8nCmLZDSGFc5yPH+5/jvB7mfTyQ="
-KEYWORDS = "eNotTlGSQyEIO8z75VJU0ceshY5C293Tb3ztDCYwJJijSlMTOmRo27h2b3UPnwfBJ\
-            vuwlZEVknGZHpP7nenIa8sZTrcp/EOFcaecPKm4rdgYaikEHecIqo7K2xC6vhTLO8\
-            k7ZBq14RzUfFJ3HEQKtaDh1mlK1wURmkhI1+kzaGk3qaA/8UYrOLSAZhbsXhrlpPh\
-            9yI6Ypm7Ar+XpumHAgSSvE/gPTTtk7g=="
+HEADER = ['assert', 'complex', 'ctype', 'errno', 'fenv', 'float', 'inttypes', 'iso646', 'limits', 'locale', 'math', 'setjmp', 'signal', 'stdalign', 'stdarg', 'stdatomic', 'stdbool', 'stddef', 'stdint', 'stdio', 'stdlib', 'stdnoreturn', 'string', 'tgmath', 'threads', 'time', 'uchar', 'wchar', 'wctype']
+KEYWORDS = ['#define', '#elif', '#else', '#endif', '#if', '#ifdef', '#ifndef', '#include', '#line', '#pragma', '#undef', 'auto', 'break', 'case', 'char', 'const', 'continue', 'default', 'do', 'double', 'else', 'enum', 'extern', 'float', 'for', 'goto', 'if', 'int', 'long', 'register', 'return', 'short', 'signed', 'sizeof', 'static', 'struct', 'switch', 'typedef', 'union', 'unsigned', 'void', 'volatile', 'while']
+
 KEYWORDS = decode(KEYWORDS).split(',')
 HEADER = decode(HEADER).split(',')
 STORE = dict()  # Stores major details of program
 STORE['KEYWORDS'] = KEYWORDS
 STORE['HEADER'] = HEADER
 
-#print KEYWORDS
+print "HEADER =", HEADER
+print "KEYWORDS =", KEYWORDS
