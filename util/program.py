84dd3ca63460f2e4d3f9e64f5b8c3646bb55e590
Pulkit07 <7895pulkit@gmail.com>
2017-03-25 19:08:04 +0530

    added program class

---
+++ util/program.py
@@ -0,0 +1,15 @@
+'''This file contains the class 'program' which will be representative
+of the c program. This class will have objects of other classes as
+attributes which represents various parts of the code.'''
+
+class program:
+
+	self.functions = []
+	self.macros = []
+	self.structs = []
+	self.global_vars = []
+	self.proto_decl = []
+	self.global_comments = []
+
+	def __init__(self,filepointer):
+		pass
