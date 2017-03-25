12234046749c2d37c8d196c0ad57cc7bf6aa9551
Lavee Singh <laveechaudharytheone@gmail.com>
2017-03-25 22:32:45 +0530

    Initial for loop processor added

--- tests/test1.c
+++ tests/test1.c
@@ -2,6 +2,14 @@
 
 const int global_variable;
 
+#define ll long long
+
+void print(const char *s){
+  printf("%s",s);
+}
+
+#define ull unsigned long long
+#include <stdlib.h>
 
 int main(){
   printf("Hello World!\n");
