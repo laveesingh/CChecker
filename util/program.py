149de29182031048d0b447f0d09aa3d58a7988e1
Pulkit07 <7895pulkit@gmail.com>
2017-03-26 15:17:08 +0530

    make functions which will ease and organise the process of parsing and making class

--- util/program.py
+++ util/program.py
@@ -18,4 +18,58 @@ class program:
 		self.global_comments = []
 
 	def load_attrs(self, fp):
+		text = fp.read()
+		list_of_lines = []
+		for line in text:
+			list_of_lines.append(line)
+
+		lineno = 0
+		no_of_lines = len(list_of_lines)
+
+		while lineno < list_of_lines:
+			
+			if list_of_lines[lineno].strip() == '':
+				lineno = lineno + 1
+				continue
+
+			lineno = preprocessor(list_of_lines, lineno)
+			lineno = global_vars(list_of_lines, lineno)
+			lineno = struct(list_of_lines, lineno)
+			lineno = func_prototype(list_of_lines, lineno)
+			lineno = function(list_of_lines, lineno)
+
+
+def preprocessor(lines, lineno):
+	''''''
+	if is_preprocessor(lines[lineno]):
 		pass
+
+	return lineno
+
+def global_vars(lines, lineno):
+	''''''
+	if is_global_var(lines[lineno]):
+		pass
+
+	return lineno
+
+def function(lines, lineno):
+	''''''
+	if is_func(lines[lineno]):
+		pass
+
+	return lineno
+
+def func_prototype(lines, lineno):
+	''''''
+	if is_func_prototype(lines[lineno]):
+		pass
+
+	return lineno
+
+def struct(lines, lineno):
+	''''''
+	if is_struct(lines[lineno]):
+		pass
+
+	return lineno
