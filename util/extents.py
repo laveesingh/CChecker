177ec2e2b6cf4a501f390d2d3ef4cb35996ee820
Lavee Singh <laveechaudharytheone@gmail.com>
2017-03-28 14:11:01 +0530

    Resolved unnecessary conflicts in extent.py

---
+++ util/extents.py
@@ -0,0 +1,187 @@
+'''
+This program checks extents(from starting point till ending point) of various
+components of the program. Components include: functions, loops, conditionals,
+structs, etc.
+'''
+import re
+
+from store import counterparts, root
+import parse
+import utilities
+
+
+possible_function_list = [
+        'blank', 'comment1', 'preprocessor',
+        'function_prototype', 'function_definition', 'forloop', 'whileloop',
+        'dowhileloop', 'ifcondition', 'elseifcondition', 'elsecondition',
+        'switch', 'declaration', 'assignment', 'struct', 'union'
+]
+
+def process_line(lines_list, index):
+    '''
+    processes line according to defined function for particular type
+    then returns line number, where counter_part of this line ends
+    '''
+    statement_type = utilities.resolve(lines_list, index)
+    returned = eval(statement_type +'(lines_list, index)')
+    return returned
+
+
+def blank(lines_list, index):
+    return index
+
+def preprocessor(name, lines_list, index):
+    '''
+    Function returns index of the ending line of the preprocessor
+    '''
+    if index == len(lines_list): return index
+    one_liners = ['define', 'include', 'line', 'pragma']
+    if name in one_liners:
+        return index
+    if parse.is_preprocessor(lines_list, index):
+        thisname = re.search(r'\#(?P<name>\w+)', ).group('name')
+        if thisname == counterparts[name]:
+            return index
+        thisend = preprocessor(thisname, lines_list, index)
+        return preprocessor(name, lines_list, thisend+1)
+    else:
+        thisend = process_line(lines_list, index)
+        return preprocessor(name, lines_list, thisend+1)
+
+def function_definition(lines_list, index):
+    '''
+    handles only function of the format
+    >>> rettype func(args){
+            body
+        }
+    '''
+    if lines_list[index].strip() == '}':  #ignored inline comments for now
+        return index
+    thisend = process_line(lines_list, index)
+    return function_definition(lines_list, thisend+1)
+
+
+def forloop(lines_list, index):
+    '''
+    handles only for loops of the format
+    >>> for(details){
+            body
+        }
+    '''
+    if lines_list[index].strip() == '}':  #ignored inline comments for now
+        return index
+    thisend = process_line(lines_list, index)
+    return forloop(lines_list, thisend+1)
+
+
+def whileloop(lines_list, index):
+    '''
+    handles only while loops of the format
+    >>> while(conditions){
+            body
+        }
+    '''
+    if lines_list[index].strip() == '}':  #ignored inline comments for now
+        return index
+    thisend = process_line(lines_list, index)
+    return whileloop(lines_list, thisend+1)
+
+def dowhileloop(lines_list, index):
+    '''
+    handles only dowhile loops of the format
+    >>> do{
+            body
+        } while(condition);
+    '''
+    if lines_list[index].strip() == '}':  #ignored inline comments for now
+        return index
+    thisend = process_line(lines_list, index)
+    return dowhileloop(lines_list, thisend+1)
+
+def ifcondition(lines_list, index):
+    '''
+    handles only if conditions of the format
+    >>> if(conditions){
+            body
+        }
+    '''
+    if lines_list[index].strip() == '}':  #ignored inline comments for now
+        return index
+    thisend = process_line(lines_list, index)
+    return ifcondition(lines_list, thisend+1)
+
+
+def elseifcondition(lines_list, index):
+    '''
+    handles only else if conditions of the format
+    >>> else if(conditions){
+            body
+        }
+    '''
+    if lines_list[index].strip() == '}':  #ignored inline comments for now
+        return index
+    thisend = process_line(lines_list, index)
+    return elseifcondition(lines_list, thisend+1)
+
+
+def elsecondition(lines_list, index):
+    '''
+    handles only else conditions of the format
+    >>> else{
+            body
+        }
+    '''
+    if lines_list[index].strip() == '}':  #ignored inline comments for now
+        return index
+    thisend = process_line(lines_list, index)
+    return elsecondition(lines_list, thisend+1)
+
+
+def switch(lines_list, index):
+    '''
+    handles only switch of the format
+    >>> switch(constraint){
+            body
+        }
+    '''
+    if lines_list[index].strip() == '}':  #ignored inline comments for now
+        return index
+    thisend = process_line(lines_list, index)
+    return switch(lines_list, thisend+1)
+
+
+def struct(lines_list, index):
+    '''
+    handles only structs of the format
+    >>> struct name{
+            body
+        };
+    '''
+    if lines_list[index].strip() == '};':  #ignored inline comments for now
+        return index
+    thisend = process_line(lines_list, index)
+    return struct(lines_list, thisend+1)
+
+def union(lines_list, index):
+    '''
+    handles only structs of the format
+    >>> union name{
+            body
+        };
+    '''
+    return index
+
+def declaration(lines_list, index):
+    '''handles only declarations of the following format
+    >>> datatype var1;
+    or
+    >>> datatype var1, var2...;
+    '''
+    return index
+
+def assignment(lines_list, index):
+    '''handles only assignments of the following format
+    >>> datatype var1 = val1;
+    or any other inline assignment
+    '''
+    return index
