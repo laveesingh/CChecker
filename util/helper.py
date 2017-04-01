e6af251c0e233f45a1959dfafbf60e8c9febaec0
Lavee Singh <laveechaudharytheone@gmail.com>
2017-04-01 23:49:11 +0530

    unions variables parser function added into helper.py

--- util/helper.py
+++ util/helper.py
@@ -75,6 +75,40 @@ def parse_struct_vars(program_instance):
         struct.vars = vars_dict
 
 
+def parse_union_vars(program_instance):
+    '''
+    Takes a program instance and returns a dictionary containing all varibles
+    within all structures along with their datatypes
+    '''
+    modifiers = r'(?P<mod>(const|auto|static|register|extern|volatile|signed|unsigned| )*)'
+    fpattern = r'' + modifiers + '\s+(?P<type>' 
+    spattern = ')\*{0,2}\s+.*?(?P<name>\w+)'
+    for union in program_instance.unions:
+        vars_dict = {}
+        for text_line in union.text:
+            match = None
+            for pos_dtype in bd:
+                pattern = fpattern + pos_dtype + spattern
+                match = re.search(pattern, text_line)
+                if match:
+                    break
+            if not match:
+                continue
+            dtype = match.group('type')
+            varname = match.group('name')
+            unsigned = 0
+            if match.group('mod') is not None and 'unsigned' in match.group('mod'):
+                unsigned = 1
+            vars_dict[varname] = (dtype, unsigned) # unsigned is 1 if dtype is unsigned, 0 otherwise
+            csv = text_line.split(',')[1:]
+            if csv:
+                for declaration in csv:
+                    pat = r'(?P<name>\w+).*'
+                    match = re.search(pat, declaration)
+                    vars_dict[match.group('name')] = (dtype, unsigned)
+        union.vars = vars_dict
+
+
 condition_st = ('if', 'else if', 'while')
 loops = ('for')
 
